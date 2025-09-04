5677efce593c8c9f6a3e487c66be3c7fb7bc9358
gelday <geladay18@gmail.com>
2025-09-04 12:46:31 +0800

    Create schedule.php

---
+++ schedule.php
@@ -0,0 +1,11 @@
+<?php
+$schedule = [
+    ["time" => "07:30", "end" => "09:00", "subject" => "EVD", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "09:00", "end" => "10:30", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "10:30", "end" => "12:00", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "12:00", "end" => "3:00", "subject" => "Vacant Time", "teacher" => ""],
+    ["time" => "3:00", "end" => "4:30", "subject" => "NET", "teacher" => "Ms. Gladymay Sadorra"],
+    ["time" => "4:30", "end" => "6:00", "subject" => "SPI", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "6:00", "end" => "7:30", "subject" => "ELEC", "teacher" => "Ms. En Catarungan"],
+    ["time" => "7:30", "end" => "9:00", "subject" => "FREE ELEC 1", "teacher" => "Mr. Jade Louis Cabucos"],
+];
