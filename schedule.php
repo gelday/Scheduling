13735e825ed3fd36ace7a19ede4c64ac41f7315e
Annalyn Carali <annalili065@gmail.com>
2025-09-05 11:27:38 +0800

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -2,10 +2,10 @@
 $schedule = [
     ["time" => "07:30", "end" => "09:00am", "subject" => "EVD", "teacher" => "Mr. Jade Louis Cabucos"],
     ["time" => "09:01", "end" => "10:30am", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
-    ["time" => "10:31", "end" => "12:00pm", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "10:31", "end" => "12:00pm", "subject" => "SIA", "teacher" => "Mr. Yestin Roy A. Prado"],
     ["time" => "12:01", "end" => "2:59pm", "subject" => "VACANT TIME", "teacher" => "NONE"],
     ["time" => "3:00", "end" => "4:30pm", "subject" => "NET", "teacher" => "Ms. Gladymay Sadorra"],
-    ["time" => "4:31", "end" => "6:00pm", "subject" => "SPI", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "4:31", "end" => "6:00pm", "subject" => "SPI", "teacher" => "Mr. Yestin Roy A. Prado"],
     ["time" => "6:01", "end" => "7:30pm", "subject" => "ELEC", "teacher" => "Ms. En Catarungan"],
     ["time" => "7:31", "end" => "9:00pm", "subject" => "FREE ELEC 1", "teacher" => "Mr. Jade Louis Cabucos"],
 ];
