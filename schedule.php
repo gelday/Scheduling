fa578404499efa022a0d027d448509564dca8fa2
renalinekemebaru <renalinemalazarte8@gmail.com>
2025-09-05 11:25:12 +0800

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -1,9 +1,9 @@
 <?php
 $schedule = [
     ["time" => "07:30", "end" => "09:00am", "subject" => "EVD", "teacher" => "Mr. Jade Louis Cabucos"],
-    ["time" => "09:00", "end" => "10:30am", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
-    ["time" => "10:30", "end" => "12:00pm", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
-    ["time" => "12:00", "end" => "2:59pm", "subject" => "VACANT TIME", "teacher" => "NONE"],
+    ["time" => "09:01", "end" => "10:30am", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "10:31", "end" => "12:00pm", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "12:01", "end" => "2:59pm", "subject" => "VACANT TIME", "teacher" => "NONE"],
     ["time" => "3:00", "end" => "4:30pm", "subject" => "NET", "teacher" => "Ms. Gladymay Sadorra"],
     ["time" => "4:31", "end" => "6:00pm", "subject" => "SPI", "teacher" => "Mr. Yestin Roy Prado"],
     ["time" => "6:01", "end" => "7:30pm", "subject" => "ELEC", "teacher" => "Ms. En Catarungan"],
