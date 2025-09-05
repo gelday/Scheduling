1913ca7dced142bca8ac6b1cdf25eaa2aec40fd0
Angelyn B Bustamante <elynx.xoxo@gmail.com>
2025-09-05 11:20:06 +0800

    Update schedule.php

--- schedule.php
+++ schedule.php
@@ -1,11 +1,11 @@
 <?php
 $schedule = [
-    ["time" => "07:30", "end" => "09:00", "subject" => "EVD", "teacher" => "Mr. Jade Louis Cabucos"],
-    ["time" => "09:00", "end" => "10:30", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
-    ["time" => "10:30", "end" => "12:00", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
-    ["time" => "12:00", "end" => "3:00", "subject" => "Vacant Time", "teacher" => ""],
-    ["time" => "3:00", "end" => "4:30", "subject" => "NET", "teacher" => "Ms. Gladymay Sadorra"],
-    ["time" => "4:30", "end" => "6:00", "subject" => "SPI", "teacher" => "Mr. Yestin Roy Prado"],
-    ["time" => "6:00", "end" => "7:30", "subject" => "ELEC", "teacher" => "Ms. En Catarungan"],
-    ["time" => "7:30", "end" => "9:00", "subject" => "FREE ELEC 1", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "07:30", "end" => "09:00am", "subject" => "EVD", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "09:00", "end" => "10:30am", "subject" => "IAS", "teacher" => "Mr. Jade Louis Cabucos"],
+    ["time" => "10:30", "end" => "12:00pm", "subject" => "SIA", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "12:00", "end" => "2:59pm", "subject" => "VACANT TIME", "teacher" => "NONE"],
+    ["time" => "3:00", "end" => "4:30pm", "subject" => "NET", "teacher" => "Ms. Gladymay Sadorra"],
+    ["time" => "4:31", "end" => "6:00pm", "subject" => "SPI", "teacher" => "Mr. Yestin Roy Prado"],
+    ["time" => "6:01", "end" => "7:30pm", "subject" => "ELEC", "teacher" => "Ms. En Catarungan"],
+    ["time" => "7:31", "end" => "9:00pm", "subject" => "FREE ELEC 1", "teacher" => "Mr. Jade Louis Cabucos"],
 ];
